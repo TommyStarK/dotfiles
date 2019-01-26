5a0f2c446ad5c58b155c18412b24e7e4c0b1d0bd
Thomas Milox <thomasmilox@gmail.com>
2019-01-26 18:42:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -70,20 +70,25 @@ brew install gdb
 brew install go
 brew install mongodb
 brew install mysql
+brew install redis
 brew install node
 brew install python@2
 brew install python
-# install python 3.6.5 (package like xgboost not compatible with 3.7 so far)
-sudo mkdir /usr/local/Frameworks
-sudo chown $(whoami):admin /usr/local/Frameworks
-brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
+brew cask install docker
+brew cask install google-chrome
+brew cleanup
 
 # Install nano with syntax highlighting
 brew install nano
 wget https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh -O- | sh
 
+# Install nvm
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
 # Install Python deps
-pip3.6 install -r requirements.txt --upgrade --user
+pip2 install --upgrade pip
+pip3 install --upgrade pip
+pip3 install -r requirements.txt --upgrade --user
 
 # Install Node modules
 npm install -g cors
@@ -92,14 +97,6 @@ npm install -g express
 npm install -g mongodb
 npm install -g react
 npm install -g create-react-app
-
-# Install Docker
-brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
-sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-
-# Remove outdated versions from the cellar.
-brew cleanup
 
 # Setup MacOs defaults
 chmod +x .macos
