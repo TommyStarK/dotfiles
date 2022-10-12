5de8928a623b87004b9eb55d0b2f05191a78fd72
TommyStarK <thomasmilox@gmail.com>
2022-10-12 14:13:44 +0200

    perf: load gnubin faster
    Signed-off-by: TommyStarK <thomasmilox@gmail.com>

--- setup.sh
+++ setup.sh
@@ -77,8 +77,8 @@ brew cleanup
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
+	echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+	chsh -s /usr/local/bin/bash;
 fi;
 
 # Copy .files to HOME
