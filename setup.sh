a65479d173d5dfffdb505d66681a7ec31d6323e7
TommyStarK <thomasmilox@gmail.com>
2022-10-12 14:11:35 +0200

    perf: load gnubin faster
    Signed-off-by: TommyStarK <thomasmilox@gmail.com>

--- setup.sh
+++ setup.sh
@@ -77,8 +77,8 @@ brew cleanup
 
 # Switch to using brew-installed zsh as default shell
 if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
-  echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/zsh;
+	echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells;
+	chsh -s /usr/local/bin/zsh;
 fi;
 
 for file in ./.{aliases,curlrc,editorconfig,exports,functions,gitconfig,paths,screenrc,wgetrc,zshrc}; do
